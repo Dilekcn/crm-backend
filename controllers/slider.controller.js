6c2176fa6b1e620485904bf3321898fc85d0bdae
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-05-16 23:12:52 +0100

    created getAll controller

--- controllers/slider.controller.js
+++ controllers/slider.controller.js
@@ -1,2 +1,11 @@
 const mongoose = require('mongoose');
 const SliderModel = require('../model/Slider.model');
+
+exports.getAll = async (req, res) => {
+	try {
+		const response = await SliderModel.find();
+		res.json(response);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+};
