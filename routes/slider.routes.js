40fe5ada04f2d2b4b11440e1a8596095b7750701
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-05-16 23:15:17 +0100

    added slider route

---
+++ routes/slider.routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+const SliderController = require('../controllers/slider.controller');
+
+router.get('/slider', SliderController.getAll);
