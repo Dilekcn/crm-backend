16ed8c4441f1b60b7eb1c6084de338b18f049533
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-05-16 23:06:40 +0100

    added slider model

---
+++ model/Slider.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const SliderSchema = new Schema({
+	title: { type: String, required: true, unique: true },
+	subtitle: { type: String, required: true },
+	url: { type: String, required: true },
+	buttonText: { type: String, required: true },
+	order: { type: Number, required: true },
+	isActive: { type: Boolean, default: true },
+	isDeleted: { type: Boolean, default: false },
+	mediaId: { type: String, required: true },
+	isVideo: { type: Boolean, required: true },
+});
+
+module.exports = mongoose.model('slider', SliderSchema);
