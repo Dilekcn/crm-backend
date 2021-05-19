84657417bce714cb96ece0f036f03d5a0131c318
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-05-19 12:08:13 +0100

    added roles schema

--- model/Expert.model.js
+++ model/Expert.model.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ExpertSchema = new Schema({
-	firstname: { type: String, required: true },
-	lastname: { type: String, required: true },
-	expertise: { type: String, required: true },
-	isActive: { type: Boolean, default: true },
-	isDeleted: { type: Boolean, default: false },
-});
+const ExpertSchema = new Schema(
+	{
+		firstname: { type: String, required: true },
+		lastname: { type: String, required: true },
+		expertise: { type: String, required: true },
+		isActive: { type: Boolean, default: true },
+		isDeleted: { type: Boolean, default: false },
+	},
+	{ timestamps: true },
+);
 
 module.exports = mongoose.model('expert', ExpertSchema);
