9a7d532b88abcc40e5969ebad8c00daceb3b15b1
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-06-14 22:37:54 +0100

    created components model

---
+++ model/Components.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ComponentsSchema = new Schema({
+	name: { type: String, required: true, unique: true },
+	componentId: { type: Schema.Types.ObjectId, required: true },
+	description: { type: String },
+	isActive: { type: Boolean, default: true },
+	isDeleted: { type: Boolean, default: false },
+});
+
+module.exports = mongoose.model('component', ComponentsSchema);
