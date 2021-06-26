2865204c0b9ac6d69e889e115512521f381326d1
Elif Balci <mrselifbalci@gmail.com>
2021-06-26 14:14:35 +0100

    name added to subscribers model

--- model/Subscribers.model.js
+++ model/Subscribers.model.js
@@ -4,6 +4,9 @@ const Schema = mongoose.Schema;
 const SubscribersModel = new Schema(
 	{
 		email: { type: String, unique: true },
+		name:{type:String},
+		isActive:{ type: Boolean, default: true },
+		isDeleted:{ type: Boolean, default: false }
 	},
 	{ timestamps: true }
 );
