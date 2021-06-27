5c045f4748f02aa2e4e295e9f40203a50a02ea80
Elif Balci <mrselifbalci@gmail.com>
2021-06-27 12:06:21 +0100

    cors policy updated

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+  });
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users.routes');
+
 
 const iconBoxRouter = require('./routes/iconBox.routes');
 const companyIntroductionRouter = require('./routes/companyIntroductions.routes');
