b5c65d5616a7c19ecf2d804d0da210f713423c57
Elif Balci <mrselifbalci@gmail.com>
2021-06-27 12:08:08 +0100

    cors updated

--- app.js
+++ app.js
@@ -5,11 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
-  });
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users.routes');
@@ -37,6 +33,11 @@ const companyProfileRouter = require('./routes/companyProfile.routes');
 // const verifyToken = require('./auth/verifyToken');
 
 var app = express();
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+  });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
