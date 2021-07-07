cf7e332f7f95121a3dc19e6dec11540bc66b47d5
Elif Balci <mrselifbalci@gmail.com>
2021-07-07 19:04:10 +0100

    .

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ const companyProfileRouter = require('./routes/companyProfile.routes');
 var app = express();
 app.use(function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
+	res.header("Access-Control-Allow-Credentials", true);
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 	res.header(
 		'Access-Control-Allow-Headers',
 		'Origin, X-Requested-With, Content-Type, Accept'
