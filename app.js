5840e9eb0f266eaebd852e18d5ed7eb7d05951e1
Elif Balci <mrselifbalci@gmail.com>
2021-07-07 18:38:03 +0100

    users

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const fileUpload = require('express-fileupload');
-// const cors = require('cors');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users.routes');
@@ -38,13 +38,13 @@ var app = express();
 // 	next();
 // });
 
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", '*');
-	res.header("Access-Control-Allow-Credentials", true);
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-	res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-	next();
-  });
+// app.use(function(req, res, next) {
+// 	res.header("Access-Control-Allow-Origin", '*');
+// 	res.header("Access-Control-Allow-Credentials", true);
+// 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+// 	res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+// 	next();
+//   });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -59,7 +59,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
-// app.use(cors());
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/', usersRouter);
