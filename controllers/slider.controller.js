d15aee92ff2522e31b6c13d10dd14f8eccd10787
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-07-12 04:40:53 +0100

    updated slider create and update controllers for media

--- controllers/slider.controller.js
+++ controllers/slider.controller.js
@@ -19,18 +19,64 @@ exports.getAllSlides = async (req, res) => {
 };
 
 exports.createSlide = async (req, res) => {
-	const data = async (data) => {
-		const newMedia = await new MediaModel({
-			title: 'slider',
-			url: data.Location || null,
-			mediaKey: data.Key,
-			alt: req.body.alt || null,
-		});
+	if (req.files) {
+		const data = async (data) => {
+			const newMedia = await new MediaModel({
+				title: 'slider',
+				url: data.Location || null,
+				mediaKey: data.Key,
+				alt: req.body.alt || null,
+			});
 
-		newMedia.save();
+			newMedia.save();
 
-		const { title, subtitle, url, buttonText, order, isActive, isDeleted, isVideo } =
-			req.body;
+			const {
+				title,
+				subtitle,
+				url,
+				buttonText,
+				order,
+				isActive,
+				isDeleted,
+				isVideo,
+			} = req.body;
+
+			const newSlide = await new SliderModel({
+				title,
+				subtitle,
+				url,
+				buttonText,
+				order,
+				isActive,
+				isDeleted,
+				mediaId: newMedia._id,
+				isVideo,
+			});
+			newSlide
+				.save()
+				.then((response) =>
+					res.json({
+						status: 200,
+						message: 'Added new slide successfully.',
+						response,
+					})
+				)
+				.catch((error) => res.json({ status: 404, message: error }));
+		};
+
+		await S3.uploadNewMedia(req, res, data);
+	} else if (req.body.mediaId) {
+		const {
+			title,
+			subtitle,
+			url,
+			buttonText,
+			order,
+			isActive,
+			isDeleted,
+			isVideo,
+			mediaId,
+		} = req.body;
 
 		const newSlide = await new SliderModel({
 			title,
@@ -40,7 +86,7 @@ exports.createSlide = async (req, res) => {
 			order,
 			isActive,
 			isDeleted,
-			mediaId: newMedia._id,
+			mediaId,
 			isVideo,
 		});
 		newSlide
@@ -53,9 +99,53 @@ exports.createSlide = async (req, res) => {
 				})
 			)
 			.catch((error) => res.json({ status: 404, message: error }));
-	};
+	} else {
+		const data = async (data) => {
+			const newMedia = await new MediaModel({
+				title: 'slider',
+				url: data.Location || null,
+				mediaKey: data.Key,
+				alt: req.body.alt || null,
+			});
 
-	await S3.uploadNewMedia(req, res, data);
+			newMedia.save();
+
+			const {
+				title,
+				subtitle,
+				url,
+				buttonText,
+				order,
+				isActive,
+				isDeleted,
+				isVideo,
+			} = req.body;
+
+			const newSlide = await new SliderModel({
+				title,
+				subtitle,
+				url,
+				buttonText,
+				order,
+				isActive,
+				isDeleted,
+				mediaId: newMedia._id,
+				isVideo,
+			});
+			newSlide
+				.save()
+				.then((response) =>
+					res.json({
+						status: 200,
+						message: 'Added new slide successfully.',
+						response,
+					})
+				)
+				.catch((error) => res.json({ status: 404, message: error }));
+		};
+
+		await S3.uploadNewMedia(req, res, data);
+	}
 };
 
 exports.getSingleSlide = async (req, res) => {
@@ -79,10 +169,10 @@ exports.getSingleSlideByTitle = async (req, res) => {
 };
 
 exports.updateSlider = async (req, res) => {
-	await SliderModel.findById({ _id: req.params.slideid })
-		.then(async (slider) => {
-			await MediaModel.findById({ _id: slider.mediaId })
-				.then(async (media) => {
+	if (req.files) {
+		await SliderModel.findById({ _id: req.params.slideid })
+			.then(async (slider) => {
+				await MediaModel.findById({ _id: slider.mediaId }).then(async (media) => {
 					const data = async (data) => {
 						await MediaModel.findByIdAndUpdate(
 							{ _id: slider.mediaId },
@@ -91,58 +181,75 @@ exports.updateSlider = async (req, res) => {
 									title: 'slider',
 									url: data.Location || null,
 									mediaKey: data.Key,
-									alt: req.body.alt || null,
+									alt: req.body.title || null,
 								},
 							},
 							{ useFindAndModify: false, new: true }
-						)
-							.then(async (updatedMedia) => {
-								const {
-									title,
-									subtitle,
-									url,
-									buttonText,
-									order,
-									isActive,
-									isDeleted,
-									isVideo,
-								} = req.body;
-								return await SliderModel.findByIdAndUpdate(
-									{ _id: req.params.slideid },
-									{
-										$set: {
-											title,
-											subtitle,
-											url,
-											buttonText,
-											order,
-											isActive,
-											isDeleted,
-											mediaId: slider.mediaId,
-											isVideo,
-										},
-									},
-									{ useFindAndModify: false, new: true }
-								)
-									.then((data) => res.json({ status: 200, data }))
-									.catch((err) =>
-										res.json({ status: 404, message: err })
-									);
-							})
-							.then((response) =>
-								res.json({
-									status: 200,
-									message: 'Slide updated successfully',
-									response,
-								})
-							);
+						).catch((err) => res.json({ status: 404, message: err }));
 					};
 					await S3.updateMedia(req, res, media.mediaKey, data);
-				})
-				.catch((err) => res.json({ status: 404, message: err }));
-		})
+				});
+				const { title, subtitle, url, buttonText, order } = req.body;
+				await SliderModel.findByIdAndUpdate(
+					{ _id: req.params.slideid },
+					{
+						$set: {
+							title,
+							subtitle,
+							url,
+							buttonText,
+							order,
+							isActive: !req.body.isActive ? true : req.body.isActive,
+							isDeleted: !req.body.isDeleted ? false : req.body.isDeleted,
+							mediaId: slider.mediaId,
+							isVideo: !req.body.isVideo ? false : req.body.isVideo,
+						},
+					},
+					{ useFindAndModify: false, new: true }
+				)
+					.then((response) =>
+						res.json({
+							status: 200,
+							message: 'Slide updated successfully',
+							response,
+						})
+					)
+					.catch((err) => res.json({ status: 404, message: err }));
+			})
+			.catch((err) => res.json({ status: 404, message: err }));
+	} else {
+		await SliderModel.findById({ _id: req.params.slideid })
+			.then(async (slider) => {
+				const { title, subtitle, url, buttonText, order, mediaId } = req.body;
 
-		.catch((err) => res.json({ status: 404, message: err }));
+				await SliderModel.findByIdAndUpdate(
+					{ _id: req.params.slideid },
+					{
+						$set: {
+							title,
+							subtitle,
+							url,
+							buttonText,
+							order,
+							isActive: !req.body.isActive ? true : req.body.isActive,
+							isDeleted: !req.body.isDeleted ? false : req.body.isDeleted,
+							mediaId: !mediaId ? slider.mediaId : mediaId,
+							isVideo: !req.body.isVideo ? false : req.body.isVideo,
+						},
+					},
+					{ useFindAndModify: false, new: true }
+				)
+					.then((response) =>
+						res.json({
+							status: 200,
+							message: 'Slide updated successfully',
+							response,
+						})
+					)
+					.catch((err) => res.json({ status: 404, message: err }));
+			})
+			.catch((err) => res.json({ status: 404, message: err }));
+	}
 };
 
 exports.removeSlide = async (req, res) => {
