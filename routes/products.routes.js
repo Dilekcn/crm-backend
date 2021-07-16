97b1ad10fd2fa78f900e592068e99be7f84b00e5
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-07-16 22:23:27 +0100

    added filter to products

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -6,6 +6,7 @@ const productsControllers = require('../controllers/products.controllers');
 router.get('/products', productsControllers.getAllProducts);
 router.get('/products/:productid', productsControllers.getSingleProduct);
 router.post('/products', productsControllers.createProduct);
+router.post('/products/filter', productsControllers.getWithQuery);
 router.put('/products/:productid', productsControllers.updateSingleProduct);
 router.delete('/products/:productid', productsControllers.deleteProduct);
 
