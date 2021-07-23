cad21102f2ff4a00a7b881657b38f789f4409076
Elif Balci <mrselifbalci@gmail.com>
2021-07-23 23:01:50 +0100

    get by type added to sections

--- routes/sections.routes.js
+++ routes/sections.routes.js
@@ -5,7 +5,7 @@ const sectionsControllers = require('../controllers/sections.controllers');
 
 router.get('/sections', sectionsControllers.getAll);
 router.get('/sections/:id', sectionsControllers.getSingleSection);
-router.get('/sections/type/:secType', sectionsControllers.getSingleSection);
+router.get('/sections/type/:secType', sectionsControllers.getSingleSectionByType);
 router.post('/sections', sectionsControllers.create);
 router.put('/sections/:id', sectionsControllers.updateSection);
 router.delete('/sections/:id', sectionsControllers.removeSingleSection);
